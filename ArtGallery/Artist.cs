14ce66974514c8222160eafbac16fc91d6622729
Tinaskj <106248161+Tinaskj@users.noreply.github.com>
2022-10-16 12:35:01 +0200

    Initial commit

---
+++ ArtGallery/Artist.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtGallery
+{
+    public class Artist
+    {
+        public string firstName;
+        public string lastName;
+        public string nationality;
+        public string birthday;
+        public int age;
+        public bool male;
+        public string typeOfArt;
+
+        public Artist(string afirstName, string alastName, string anationality, string abirthday, int aage, bool amale, string atypeOfArt)
+        {
+            this.firstName = afirstName;
+            this.lastName = alastName;
+            this.nationality = anationality;
+            this.birthday = abirthday;
+            this.age = aage;
+            this.male = amale;
+            this.typeOfArt = atypeOfArt;
+        }
+
+      
+
+    }
+
+}
---
+++ ArtGallery/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ArtGallery;
+using System.Collections.Generic;
+
+Console.WriteLine("Hello, World!");
+
+Artist artist1 = new Artist("Jamie", "Wylde", "Canada", "1986.09.03", 36, true, "Painter");
+Artist artist2 = new Artist("Lucifer", "Dark", "Austria", "1993.12.05", 29, true, "Sculpturist");
+Artist artist3 = new Artist("Davina", "Rose", "Scotland", "1987.02.23", 35, false, "Sketches");
+Artist artist4 = new Artist("Lucy", "Sato", "Japan", "1982.07.15", 40, false, "Painter");
